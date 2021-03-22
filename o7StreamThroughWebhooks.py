59206d1473fed0c3039961a5a9b39dcc14ac070d
Nexity <46597698+Nexity@users.noreply.github.com>
2021-03-22 22:06:49 +0800

    Create o7StreamThroughWebhooks.py

---
+++ o7StreamThroughWebhooks.py
@@ -0,0 +1,26 @@
+#lmao why did I make this
+
+import pyautogui
+import time
+import discord
+from discord_webhook import DiscordWebhook
+import threading
+
+assad = {
+  #10 webhooks here
+  #in total or it wont work smoothly
+       }
+
+def sendwebhook(url):
+    webhook = DiscordWebhook(url=url, content='.\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n.')
+    with open("screen.png", "rb") as f:
+        webhook.add_file(file=f.read(), filename='screen.png')
+    response = webhook.execute()
+
+while True:
+    for links in assad:
+        screenshot = pyautogui.screenshot()
+        screenshot.save("screen.png")
+        t1 = threading.Thread(target=sendwebhook, args=(links,))
+        t1.start()
+        time.sleep(0.5)
