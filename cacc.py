d394ebf5e7c62451aa3f4248286e640ef1ff3d07
Nexity <87981906+Nexity@users.noreply.github.com>
2021-08-27 20:44:46 +0800

    Update cacc.py

--- cacc.py
+++ cacc.py
@@ -1,32 +1,37 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+import time
 
-emailname = "randomtestaccountBTUITERSBI"
-driver = webdriver.Firefox()
-driver.get("https://github.com/signup?user_email=" + emailname + "%40planecrazyarchive.tk&source=form-home-signup")
-wait(10)
-emailbutton = driver.find_element_by_xpath('//*[@id="email-container"]/div[2]/button')
-emailbutton.click()
-wait(3)
+emailprefix = "planecrazyarchive.tk"
+def createaccount(emailname, emailprefix, password):
+	driver = webdriver.Chrome()
+	driver.get("https://github.com/signup?user_email=" + emailname + "%40" + emailprefix + "&source=form-home-signup")
+	time.sleep(5)
+	emailbutton = driver.find_element_by_xpath('//*[@id="email-container"]/div[2]/button')
+	emailbutton.click()
+	time.sleep(1)
+	
+	passwordinput = driver.find_element_by_xpath('//*[@id="password"]')
+	passwordinput.send_keys(password)
+	time.sleep(1)
+	passwordbutton = driver.find_element_by_xpath('//*[@id="password-container"]/div[2]/button')
+	passwordbutton.click()
+	
+	time.sleep(1)
+	
+	nameinput = driver.find_element_by_xpath('//*[@id="login"]')
+	nameinput.send_keys(emailname)
+	time.sleep(1)
+	namebutton = driver.find_element_by_xpath('//*[@id="username-container"]/div[2]/button')
+	namebutton.click()
+	
+	time.sleep(1)
+	
+	receiveemailinput = driver.find_element_by_xpath('//*[@id="opt_in"]')
+	receiveemailinput.send_keys("n")
+	time.sleep(1)
+	receiveemailbutton = driver.find_element_by_xpath('//*[@id="opt-in-container"]/div[2]/button')
+	receiveemailbutton.click()
+	print(emailname + emailprefix + ":" + password)
 
-passwordinput = driver.find_element_by_xpath('//*[@id="password"]')
-passwordinput.send_keys("testpasswordTETBWETWBTEW$T")
-wait(3)
-passwordbutton = driver.find_element_by_xpath('//*[@id="password-container"]/div[2]/button')
-passwordbutton.click()
-
-wait(3)
-
-nameinput = driver.find_element_by_xpath('//*[@id="login"]')
-nameinput.send_keys(emailname)
-wait(3)
-namebutton = driver.find_element_by_xpath('//*[@id="username-container"]/div[2]/button')
-namebutton.click()
-
-wait(3)
-
-receiveemailinput = driver.find_element_by_xpath('//*[@id="opt_in"]')
-receiveemailinput.send_keys("n")
-wait(3)
-receiveemailbutton = driver.find_element_by_xpath('//*[@id="opt-in-container"]/div[2]/button')
-receiveemailbutton.click()
+print("created accounts:")
+createaccount("nexity453345345", "testaccount123")
