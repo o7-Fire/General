7ea523024ef20c11f8952311b75658505d195c14
Nexity <87981906+Nexity@users.noreply.github.com>
2021-08-27 20:47:44 +0800

    b

--- cacc.py
+++ cacc.py
@@ -8,30 +8,30 @@ def createaccount(emailname, password):
 	time.sleep(10)
 	emailbutton = driver.find_element_by_xpath('//*[@id="email-container"]/div[2]/button')
 	emailbutton.click()
-	time.sleep(1)
+	time.sleep(2)
 	
 	passwordinput = driver.find_element_by_xpath('//*[@id="password"]')
 	passwordinput.send_keys(password)
-	time.sleep(1)
+	time.sleep(2)
 	passwordbutton = driver.find_element_by_xpath('//*[@id="password-container"]/div[2]/button')
 	passwordbutton.click()
 	
-	time.sleep(1)
+	time.sleep(2)
 	
 	nameinput = driver.find_element_by_xpath('//*[@id="login"]')
 	nameinput.send_keys(emailname)
-	time.sleep(1)
+	time.sleep(2)
 	namebutton = driver.find_element_by_xpath('//*[@id="username-container"]/div[2]/button')
 	namebutton.click()
 	
-	time.sleep(1)
+	time.sleep(2)
 	
 	receiveemailinput = driver.find_element_by_xpath('//*[@id="opt_in"]')
 	receiveemailinput.send_keys("n")
-	time.sleep(1)
+	time.sleep(2)
 	receiveemailbutton = driver.find_element_by_xpath('//*[@id="opt-in-container"]/div[2]/button')
 	receiveemailbutton.click()
 	print(emailname + emailprefix + ":" + password)
 
 print("created accounts:")
-createaccount("nexity453345345", "testaccount123")
+createaccount("nexity453345345", "TVEStvYTVEBYTweybtvWTyuVTEYU")
