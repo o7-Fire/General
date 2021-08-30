172415534d60a05c712e2ad04a62c170b701f947
Nexity <87981906+Nexity@users.noreply.github.com>
2021-08-30 18:12:27 +0800

    proven to work

--- sscr.py
+++ sscr.py
@@ -1,18 +1,45 @@
 import discord
+from discord import Webhook, AsyncWebhookAdapter
+import aiohttp
+import requests
+import time
 
 TOKEN = ""
+WEBHOOK = ""
 client = discord.Client()
-
+theserver = 0
 @client.event
 async def on_ready():
   print(f'{client.user} has connected to Discord!')
 
 @client.event
 async def on_message(message):
+	global theserver
 	if message.author.id == client.user.id and message.content == "bbpq":
 		async for content in message.channel.history(limit=None).map(lambda m: m.content):
 			if "discord.gg/" in (content):
 				print(f"https://discord.gg/{content.split('discord.gg/')[1].split(' ')[0]}")
 		print("finished")
-
+		
+	if message.author.id == client.user.id and "joinserver " in message.content:
+		theinvite = message.content.split("joinserver ")[1]
+		r = requests.post("https://discordapp.com/api/v6/invites/" + theinvite, headers={'authorization': TOKEN})
+		time.sleep(8)
+		theguildjoinedid = int(r.text.split('id": "')[1].split('", "name')[0])
+		theserver = theguildjoinedid
+		print("joined successfully")
+	
+	if message.author.id == client.user.id and message.content == "gaininvites":
+		totalinvitesgained = ""
+		for guild in client.guilds:
+			if guild.id == theserver:
+				print("got guild successfully")
+				for textchannel in guild.text_channels:
+					async for content in textchannel.history(limit=None).map(lambda m: m.content):
+						if "discord.gg/" in (content):
+							totalinvitesgained = totalinvitesgained + (f"https://discord.gg/{content.split('discord.gg/')[1].split(' ')[0]}") + "\n"
+		async with aiohttp.ClientSession() as session:
+			webhook = Webhook.from_url(WEBHOOK, adapter=AsyncWebhookAdapter(session))
+			await webhook.send("```" + totalinvitesgained + "```")
+			
 client.run(TOKEN)
