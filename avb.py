fec0b23ec97172cfa9757170d67a650ff9a2f00e
Nexity <87981906+Nexity@users.noreply.github.com>
2021-08-30 10:01:30 +0800

    f

--- avb.py
+++ avb.py
@@ -1,4 +1,4 @@
-# for artic vault
+#for artic vault
 import keep_alive
 keep_alive.keep_alive()
 import os
@@ -26,16 +26,21 @@ async def on_ready():
 @client.event
 async def on_message(message):
   if message.content == "getinvite" and message.channel.id == 840041811384860709:
+    print("on it")
     async with aiohttp.ClientSession() as session:
-      webhook = Webhook.from_url('webhook_url', adapter=AsyncWebhookAdapter(session))
-      r = requests.get("https://raw.githubusercontent.com/o7-Fire/General/master/idb.txt").text.split("\n")
-      totalcount = 0
+      webhook = Webhook.from_url(os.getenv("WEBHOOK"), adapter=AsyncWebhookAdapter(session))
+      r = requests.get("https://raw.githubusercontent.com/o7-Fire/General/master/idb.txt").text.split("bbbab")[1].split("\n")
+      totalcount = 1
       totalmessage = ""
       for lines in r:
-        if totalcount % 5 == 0 and (len(r) - totalcount) >= 5:
+        print(totalcount)
+        print(lines)
+        if totalcount % 5 != 0 and (len(r) - totalcount) >= 5:
           totalmessage = totalmessage + lines + "\n"
+          totalcount = totalcount + 1
         else:
-          await webhook.send(totalmessage)
+          print("sending: \n" + totalmessage)
+          await webhook.send(content=totalmessage)
           totalmessage = ""
         
 client.run(TOKEN)
