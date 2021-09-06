506b3f5809cecb8294c809db85f03312972e628d
Nexity <89518595+NexitySecond@users.noreply.github.com>
2021-09-06 09:43:05 +0800

    fuck multithreading

--- mc/other.js
+++ mc/other.js
@@ -6,17 +6,7 @@ var bloodhoundPlugin = require('mineflayer-bloodhound')(mineflayer);
 const pvp = require('mineflayer-pvp').plugin
 var sleep = require('sleep');
 const vec3 = require('vec3')
-
-function makeid(length) {
-    var result           = '';
-    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    var charactersLength = characters.length;
-    for ( var i = 0; i < length; i++ ) {
-      result += characters.charAt(Math.floor(Math.random() * 
- charactersLength));
-   }
-   return result;
-}
+var args = process.argv.slice(2);
 
 function itemToString (item) {
 	if (item) {
@@ -26,153 +16,142 @@ function itemToString (item) {
 	}
 }
 
-var i = 1;             
+const bot = mineflayer.createBot({
+  host: 'shdif.myserver.gs',
+  username: (args[0]),
+  version: '1.16.5',
+  //port: 28136
+})
+const mcData = require('minecraft-data')(bot.version)
+var pi = 3.14159;
+const RANGE_GOAL = 1
 
-function myLoop() {       
-  setTimeout(function() { 
-	var botnumber = i;
-	const bot = mineflayer.createBot({
-	  host: '',
-	  username: ('NBot' + botnumber.toString()),
-	  version: '1.16.5',
-	  //port: 28136
-	})
-	const mcData = require('minecraft-data')(bot.version)
-	var pi = 3.14159;
-	const RANGE_GOAL = 1
-	
-	navigatePlugin(bot);
-	bot.loadPlugin(pathfinder)
-	bot.loadPlugin(pvp)
-	bloodhoundPlugin(bot);
-	bot.bloodhound.yaw_correlation_enabled = true;
-	
-	const defaultMove = new Movements(bot, mcData)
+navigatePlugin(bot);
+bot.loadPlugin(pathfinder)
+bot.loadPlugin(pvp)
+bloodhoundPlugin(bot);
+bot.bloodhound.yaw_correlation_enabled = true;
 
+const defaultMove = new Movements(bot, mcData)
+
+/*
+bot.once('spawn', () => {
+  mineflayerViewer(bot, { port: 8090, firstPerson: true }) // port is the minecraft server port, if first person is false, you get a bird's-eye view
+})
+*/
+bot.on('onCorrelateAttack', function (attacker,victim,weapon) {
+	if ((victim.displayName || victim.username).startsWith("NBot")) {
+		if ((attacker.displayName || attacker.username).startsWith("NBot")) {
+			
+		} else {
+			//bot.chat(`${(victim.displayName || victim.username)} is getting attacked by ${(attacker.displayName || attacker.username)}`)
+			bot.pvp.attack(attacker)
+		}
+	}
+});
+
+bot.on('chat', (username, message) => {
+	if (username === bot.username) return
+	if (message.startsWith("say")) {
+		bot.chat(message.replace("say ", ""))
+	}
+	
+	if (message === "bot come") {
+	const target = bot.players[username].entity
+	if (!target) {
+		bot.chat("I don't see you !")
+		return
+	}
+	const { x: playerX, y: playerY, z: playerZ } = target.position
+
+	bot.pathfinder.setMovements(defaultMove)
+	bot.pathfinder.setGoal(new GoalNear(playerX, playerY, playerZ, RANGE_GOAL))
+	}
+	
+	if (message === "xzc") {
+	
+	}
+	
 	/*
-	bot.once('spawn', () => {
-	  mineflayerViewer(bot, { port: 8090, firstPerson: true }) // port is the minecraft server port, if first person is false, you get a bird's-eye view
-	})
-	*/
-	bot.on('onCorrelateAttack', function (attacker,victim,weapon) {
-		if ((victim.displayName || victim.username).startsWith("NBot")) {
-			if ((attacker.displayName || attacker.username).startsWith("NBot")) {
-				
-			} else {
-				//bot.chat(`${(victim.displayName || victim.username)} is getting attacked by ${(attacker.displayName || attacker.username)}`)
-				bot.pvp.attack(attacker)
-			}
-		}
-	});
-
-	bot.on('chat', (username, message) => {
-		if (username === bot.username) return
-		if (message.startsWith("say")) {
-			bot.chat(message.replace("say ", ""))
-		}
-		
-		if (message === "bot come") {
+	if (message === "line up") {
 		const target = bot.players[username].entity
 		if (!target) {
-			bot.chat("I don't see you !")
-			return
+		  bot.chat("I don't see you !")
+		  return
 		}
 		const { x: playerX, y: playerY, z: playerZ } = target.position
 
 		bot.pathfinder.setMovements(defaultMove)
-		bot.pathfinder.setGoal(new GoalNear(playerX, playerY, playerZ, RANGE_GOAL))
+		bot.pathfinder.setGoal(new GoalNear(playerX + args[0].substr(-3), playerY, playerZ, RANGE_GOAL))
+	}
+	*/
+	
+	if (message.startsWith("equipblock ")) {
+		const mcData = require('minecraft-data')(bot.version)
+		let itemsByName
+		if (bot.supportFeature('itemsAreNotBlocks')) {
+			itemsByName = 'itemsByName'
+		} else if (bot.supportFeature('itemsAreAlsoBlocks')) {
+			itemsByName = 'blocksByName'
 		}
-		
-		if (message === "xzc") {
-		
+		var blocktoequip = message.replace("equipblock ", "");
+		bot.equip(mcData[itemsByName][blocktoequip].id, 'hand', (err) => {
+		if (err) {
+			bot.chat(`unable to equip ${blocktoequip}: ${err.message}`)
+		} else {
+			bot.chat(`equipped ${blocktoequip}`)
 		}
-	  
-		if (message === "line up") {
-			const target = bot.players[username].entity
-			if (!target) {
-			  bot.chat("I don't see you !")
-			  return
-			}
-			const { x: playerX, y: playerY, z: playerZ } = target.position
-
-			bot.pathfinder.setMovements(defaultMove)
-			bot.pathfinder.setGoal(new GoalNear(playerX + botnumber, playerY, playerZ, RANGE_GOAL))
-		}
-		
-		if (message.startsWith("equipblock ")) {
-			const mcData = require('minecraft-data')(bot.version)
-			let itemsByName
-			if (bot.supportFeature('itemsAreNotBlocks')) {
-				itemsByName = 'itemsByName'
-			} else if (bot.supportFeature('itemsAreAlsoBlocks')) {
-				itemsByName = 'blocksByName'
-			}
-			var blocktoequip = message.replace("equipblock ", "");
-			bot.equip(mcData[itemsByName][blocktoequip].id, 'hand', (err) => {
-			if (err) {
-				bot.chat(`unable to equip ${blocktoequip}: ${err.message}`)
-			} else {
-				bot.chat(`equipped ${blocktoequip}`)
-			}
+		})
+	}
+	
+	if (message.startsWith("abuild ")) {
+		const messagesplit = message.split(" ") // below, 0 -1 1
+		const referenceBlock = bot.blockAt(bot.entity.position.offset(messagesplit[1], messagesplit[2], messagesplit[3]))
+		bot.on('move', pb)
+		function pb () {
+			bot.placeBlock(referenceBlock, vec3(0, 0, 1), (err) => {
+				if (err) {
+					console.log('cant')
+				}
 			})
 		}
-		
-		if (message.startsWith("abuild ")) {
-			const messagesplit = message.split(" ") // below, 0 -1 1
-			const referenceBlock = bot.blockAt(bot.entity.position.offset(messagesplit[1], messagesplit[2], messagesplit[3]))
-			bot.on('move', pb)
-			function pb () {
-				bot.placeBlock(referenceBlock, vec3(0, 0, 1), (err) => {
-					if (err) {
-						console.log('cant')
-					}
-				})
-			}
-		}
-		
-		if (message === "attackm") {
-			const target = bot.players[username].entity
-			looptarget()
-			function looptarget () {
-				setTimeout(function() {
-					bot.navigate.to(target.position);
-					
-					bot.attack(target)
-					looptarget()
-				}, 1000)
-			}
-		}
-		
-		if (message === "tellinventory") {
-			items = bot.inventory.items()
-			const output = items.map(itemToString).join(', ')
-			if (output) {
-				bot.chat(output)
-			} else {
-				bot.chat('empty')
-			}
-		}
-
-	})
+	}
 	
-	/*
-	bot.on('end', function () {
-		console.log("Disconnected. Waiting 5 seconds")
-		sleep.sleep(5);
-		const shell = require('shelljs')
-		shell.exec('node main.js')
-		throw new Error("rejoining");
-	});
-	*/
+	if (message === "attackm") {
+		const target = bot.players[username].entity
+		looptarget()
+		function looptarget () {
+			setTimeout(function() {
+				bot.navigate.to(target.position);
+				
+				bot.attack(target)
+				looptarget()
+			}, 1000)
+		}
+	}
+	
+	if (message === "tellinventory") {
+		items = bot.inventory.items()
+		const output = items.map(itemToString).join(', ')
+		if (output) {
+			bot.chat(output)
+		} else {
+			bot.chat('empty')
+		}
+	}
 
-	// Log errors and kick reasons:
-	bot.on('kicked', console.log)
-	bot.on('error', console.log)
-    i++;               
-    if (i <= 10) { 
-      myLoop();             
-    }              
-  }, 6000)
-}
+})
+	
+/*
+bot.on('end', function () {
+	console.log("Disconnected. Waiting 5 seconds")
+	sleep.sleep(5);
+	const shell = require('shelljs')
+	shell.exec('node main.js')
+	throw new Error("rejoining");
+});
+*/
 
-myLoop();
+// Log errors and kick reasons:
+bot.on('kicked', console.log)
+bot.on('error', console.log)
