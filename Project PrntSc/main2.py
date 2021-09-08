06f190299796cce3741d49a3b3bec63257678fdc
Nexity <89518595+NexitySecond@users.noreply.github.com>
2021-09-08 11:10:06 +0800

    Update main2.py

--- Project PrntSc/main2.py
+++ Project PrntSc/main2.py
@@ -1,15 +1,15 @@
-#generates new screenshots, set S to 6 for old screenshots
+#generates new screenshots
 import requests
 import string
 import random
 import time
 import os
 
-S = 7
+S = 6
 while True:
   finalmessage = ""
   for i in range(5):
     ran = ''.join(random.choices(string.ascii_lowercase + string.digits, k = S))
-    finalmessage = finalmessage + f"https://prnt.sc/{ran}\n"
+    finalmessage = finalmessage + f"https://prnt.sc/1{ran}\n" #7 digit only started with 1
   requests.post(os.getenv("webhook"), {"content":finalmessage})
   time.sleep(1)
