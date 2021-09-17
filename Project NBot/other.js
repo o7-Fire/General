e1afde925d9daf8d7713ea4cd206180b9b71dd3b
shadow01148 <76457846+shadow01148@users.noreply.github.com>
2021-09-18 00:03:51 +0800

    testing

--- Project NBot/other.js
+++ Project NBot/other.js
@@ -306,6 +306,76 @@ bot.on('onCorrelateAttack', function (attacker,victim,weapon) {
 	}
 });
 
+let guardPos = null
+
+// Assign the given location to be guarded
+function guardArea (pos) {
+  guardPos = pos
+
+  // We we are not currently in combat, move to the guard pos
+  if (!bot.pvp.target) {
+    moveToGuardPos()
+  }
+}
+
+// Cancel all pathfinder and combat
+function stopGuarding () {
+  guardPos = null
+  bot.pvp.stop()
+  bot.pathfinder.setGoal(null)
+}
+
+// Pathfinder to the guard position
+function moveToGuardPos () {
+  const mcData = require('minecraft-data')(bot.version)
+  bot.pathfinder.setMovements(new Movements(bot, mcData))
+  bot.pathfinder.setGoal(new goals.GoalBlock(guardPos.x, guardPos.y, guardPos.z))
+}
+
+// Called when the bot has killed it's target.
+bot.on('stoppedAttacking', () => {
+  if (guardPos) {
+    moveToGuardPos()
+  }
+})
+
+// Check for new enemies to attack
+bot.on('physicsTick', () => {
+  if (!guardPos) return // Do nothing if bot is not guarding anything
+
+  // Only look for mobs within 16 blocks
+  const filter = e => e.type === 'mob' && e.position.distanceTo(bot.entity.position) < 20 &&
+                    e.mobType !== 'Armor Stand' // Mojang classifies armor stands as mobs for some reason?
+
+  const entity = bot.nearestEntity(filter)
+  if (entity) {
+    // Start attacking
+    bot.pvp.attack(entity)
+  }
+})
+
+// Listen for player commands
+bot.on('chat', (username, message) => {
+  // Guard the location the player is standing
+  if (message === 'bot guard') {
+    const player = bot.players[username]
+
+    if (!player) {
+      bot.chat("I can't see you.")
+      return
+    }
+
+    bot.chat('I will guard that location.')
+    guardArea(player.entity.position)
+  }
+
+  // Stop guarding
+  if (message === 'bot stop guard') {
+    bot.chat('I will no longer guard this area.')
+    stopGuarding()
+  }
+})
+
 function autototem() {
 	setTimeout(function() {
 		var ifhas = 0
