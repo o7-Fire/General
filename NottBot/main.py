33915ca6258dad9a0c09d272207a3cab3358ecb9
Nexity <89518595+NexitySecond@users.noreply.github.com>
2021-11-06 18:47:59 +0800

    zxc

--- NottBot/main.py
+++ NottBot/main.py
@@ -3,9 +3,16 @@ import threading
 import requests
 import random
 import csv
+from fake_headers import Headers
 
+header = Headers(
+    browser="chrome",  # Generate only Chrome UA
+    os="win",  # Generate ony Windows platform
+    headers=True  # generate misc headers
+)
+	    
 min = 0
-max = 2900000000 #latest account 2.9 bil
+max = 29000000 #latest account 2.9 bil
 headers = { 'User-Agent': 'Mozilla/5.0 (Windows NT 6.0; WOW64; rv:24.0) Gecko/20100101 Firefox/24.0' }
 with open("proxies.txt", "r") as proxyx:
 	proxies = proxyx.read().split("\n")
@@ -15,7 +22,7 @@ def write(message):
 		writer = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
 		writer.writerow(message)
 	
-for abasrbaebwa in range(100):
+for abasrbaebwa in range(500):
 	def newthread(threadcxount):
 		
 		while True:
@@ -29,6 +36,7 @@ for abasrbaebwa in range(100):
 			
 			thenum = random.randint(min, max)
 			finalmessage = ""
+			userids2 = str((thenum + 1) * 100 + 1)
 			userids = str((thenum + 1)*100 + 1)
 			for x in range(99):
 				userids += "%2C" + str((x + 1) + (thenum + 1)*100 + 1)
@@ -36,27 +44,31 @@ for abasrbaebwa in range(100):
 			#https://thumbnails.roblox.com/v1/users/avatar-headshot?userIds=500555555%2C500555556&size=48x48&format=Png&isCircular=false
 			#refer to this
 			isproxygood = True
-			r = requests.get(api, headers=headers, proxies=proxyDict)
-			
-			if isproxygood:
-				ther = r.json()
-				didratelimit = True
-				try:
-					bhdfbh = ther["data"]
-					didratelimit = False
-				except:
-					print("ratelimited")
-					time.sleep(10)
-					
-				if didratelimit == False:
-					for d in ther["data"]:
-						cantry = True
-						try:
-							int(d['targetId']) + 1
-						except:
-							cantry = False
-						if cantry:
-							write([d['targetId'], d['imageUrl']])
+			try:
+				r = requests.get(api, headers=header.generate(), proxies=proxyDict)
+				
+				if isproxygood:
+					ther = r.json()
+					didratelimit = True
+					try:
+						bhdfbh = ther["data"]
+						didratelimit = False
+					except:
+						print("ratelimited")
+						time.sleep(30)
+						
+					if didratelimit == False:
+						for d in ther["data"]:
+							cantry = True
+							try:
+								int(d['targetId']) + 1
+							except:
+								cantry = False
+							if cantry:
+								write([d['targetId'], d['imageUrl']])
+						print("done " + userids2)
+			except:
+				print("request failed")
 
 	t1 = threading.Thread(target=newthread, args=(abasrbaebwa,))
 	t1.start()
