d328a193a41857027cc5ea12acfafe985c06f36e
Nexity <89518595+NexitySecond@users.noreply.github.com>
2021-11-18 09:07:48 +0800

    Create logintoken.js

---
+++ logintoken.js
@@ -0,0 +1,12 @@
+let token = "your token";
+
+function login(token) {
+    setInterval(() => {
+      document.body.appendChild(document.createElement `iframe`).contentWindow.localStorage.token = `"${token}"`
+    }, 50);
+    setTimeout(() => {
+      location.reload();
+    }, 2500);
+  }
+
+login(token);
